fa4118457c2175a01476ae1a76f76f783e31a622
keyflight <olivier@andev.nl>
2019-04-28 10:46:36 +0700

    Initial commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,14 +1,59 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import {injectGlobal} from 'emotion'
+import {Canvas, Heading, Paragraph, Box} from '@cmds/demo-utils'
+import SegmentedControl from '../../src'
 
-import Example from '../../src'
+injectGlobal`
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+        margin: 0;
+    }
+`
 
 class Demo extends Component {
+
+  state = {
+    value: 'opt1'
+  }
+
   render() {
-    return <div>
-      <h1>segmented-control Demo</h1>
-      <Example/>
-    </div>
+    return (
+        <Canvas>
+          <Heading>
+            Default
+          </Heading>
+          <Box>
+            <SegmentedControl
+                value={this.state.value}
+                options={[{
+                  id: 'opt1',
+                  name: 'Option A'
+                }, {
+                  id: 'opt2',
+                  name: 'Option B'
+                }]}
+                onChange={({value}) => {
+
+                  this.setState({
+                    value
+                  })
+                }}
+            />
+          </Box>
+          <Paragraph>
+            State
+          </Paragraph>
+          <Box>
+            <pre>
+              {JSON.stringify(this.state, null, 2)}
+            </pre>
+          </Box>
+        </Canvas>
+    )
   }
 }
 
